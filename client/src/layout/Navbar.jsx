0d890dd447bad6e487120d428b10ac44b2cb4b0d
aritro66 <ghosharitro66@gmail.com>
2023-04-07 17:38:07 +0530

    nav edit

--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -60,7 +60,9 @@ export default function Navbar() {
             </Typography>
           </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
+          <Box
+            sx={{ flexGrow: 0, display: user.isLoggedIn ? "block" : "none" }}
+          >
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
